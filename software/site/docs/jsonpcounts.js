b584940b91838b76e4ccea52a861e98dea0b50bb
David Tarrant <davetaz@theodi.org>
2024-02-13 16:35:48 +0000

    Adding the build of the site

---
+++ software/site/docs/jsonpcounts.js
@@ -0,0 +1,5 @@
+
+    COUNTS = '{"Type": 3, "Property": 4, "Datatype": -1, "Enumeration": 0, "Enumerationvalue": 0, "All": 6, "schemaorgversion": "1.0"}';
+
+    insertschemacounts ( COUNTS );
+    

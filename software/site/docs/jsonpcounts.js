7d150d219f39dc029d6f83bb99a217fd5a442c1f
David Tarrant <davetaz@theodi.org>
2024-02-21 16:01:00 +0000

    Removing built site

--- software/site/docs/jsonpcounts.js
+++
@@ -1,5 +0,0 @@
-
-    COUNTS = '{"Type": 3, "Property": 4, "Datatype": -1, "Enumeration": 0, "Enumerationvalue": 0, "All": 6, "schemaorgversion": "1.0"}';
-
-    insertschemacounts ( COUNTS );
-    
